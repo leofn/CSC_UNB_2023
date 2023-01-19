64cf976ae9e7098417d059e12631b4fe37fa5447
Leonardo F. Nascimento <leofn3@gmail.com>
2023-01-18 22:16:35 -0700

    scraper

---
+++ Scraper_SOL/scraper.R
@@ -0,0 +1,20 @@
+library(rvest)
+library(tidyverse)
+
+
+i <- (0:200)
+
+urlbase <- "https://repositorio.unb.br/handle/10482/826?offset=0"
+
+# Use the read_html function to read the website
+html <- read_html("https://repositorio.unb.br/handle/10482/826?offset=0")
+
+webpage <- xml2::read_html(urlbase)
+# Extract the URLs
+url <- html %>%
+  rvest::html_nodes(xpath = "//strong/a") %>%
+  rvest::html_attr("href")
+
+url2 <- paste("https://repositorio.unb.br", url, sep = "")
+
+html2 <- read
