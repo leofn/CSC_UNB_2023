6a92bfb34714f2ef2a943167ccbc447b809cdcba
Leonardo F. Nascimento <leofn3@gmail.com>
2023-01-21 10:17:40 -0700

    second update

--- Scraper_SOL/scraper.R
+++ Scraper_SOL/scraper.R
@@ -9,7 +9,6 @@ urlbase <- "https://repositorio.unb.br/handle/10482/826?offset=0"
 # Use the read_html function to read the website
 html <- read_html("https://repositorio.unb.br/handle/10482/826?offset=0")
 
-webpage <- xml2::read_html(urlbase)
 # Extract the URLs
 url <- html %>%
   rvest::html_nodes(xpath = "//strong/a") %>%
