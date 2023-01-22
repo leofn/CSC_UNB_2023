198e78a9f0652f20b047747878b71fb449c24f07
Leonardo F. Nascimento <leofn3@gmail.com>
2023-01-21 22:43:08 -0700

    scraper and 50% part1

--- README.md
+++ README.md
@@ -1,6 +1,17 @@
 # CSC_UNB_2023
 
-- Apresentação e materiais do curso "Introdução à Ciência Social Computacional conceitos, ferramentas e desafios"
+- O que: apresentação e materiais do curso "Introdução à Ciência Social Computacional conceitos, ferramentas e desafios"
 - Quando: 23 e 25/01/2023 - 14h às 18h
 - Onde: Programa de Pós-Graduação em Sociologia UnB
-- Prof. Dr. Leonardo Nascimento (LABHD/UFBA)
+- Quem: Prof. Dr. Leonardo Nascimento (LABHD/UFBA)
+
+
+### Questionário:
+
+- Breve questionário para um apresentação prévia dos participantes: https://forms.gle/7ZTKqmtkkuM5whsQ7
+
+### Sugestões de leitura:
+
+- Introdução do livro "SALGANIK, M. J. Bit by Bit: Social Research in the Digital Age. Princeton University Press, 2019." Poderá ser lida [aqui](https://www.bitbybitbook.com/en/1st-ed/preface/)
+
+- [NASCIMENTO, L. Sociologia digital: uma breve introdução. Salvador: EDUFBA, 2020](https://repositorio.ufba.br/handle/ri/32746) 
--- Scraper_SOL/scraper.R
+++ Scraper_SOL/scraper.R
@@ -1,19 +1,50 @@
+rm(list = ls())
 library(rvest)
 library(tidyverse)
+library(glue)
+
+#### PATH
+path_base <- "https://repositorio.unb.br/handle/10482/826?offset=XXX"
+### df vazio
+df <- data.frame()
+## sequencia de paginas 
+seq <- seq(0,200,20)
+## loop para pegar os links em uma pagina
+for(i in seq){
+  print(i)
+  path <- gsub("XXX", i, path_base)
+  href <- read_html(path) %>% 
+  rvest::html_nodes(xpath = "//strong/a") %>% 
+  html_attr("href")
+  df <- rbind(df, cbind(href))
+  }
+
+## links completos para cada tese/dissertação
+links <- df$href
+links <- paste("https://repositorio.unb.br", links, sep = "")
+
+### loop para pegar os links das teses/dissertações 
+
+df2 <- data.frame()
+
+for (j in links) {
+ pdf <- read_html(j) %>% 
+  rvest::html_nodes("a") %>% 
+  html_attr("href") %>% 
+  as_tibble() %>% 
+  filter(str_detect(value, "bitstream")) %>% 
+  distinct() %>% 
+  pull()
+### link completo para o pdf
+pdf_full <- paste("https://repositorio.unb.br", pdf, sep = "")
+print(pdf_full)
 
 
-i <- seq(0,200,20)
+### tentando manter o mesmo título
 
-urlbase <- "https://repositorio.unb.br/handle/10482/826?offset=0"
+download.file(url = glue("{pdf_full}"), destfile = "Downloads/")
+ 
 
-# Use the read_html function to read the website
-html <- read_html("https://repositorio.unb.br/handle/10482/826?offset=0")
 
-# Extract the URLs
-url <- html %>%
-  rvest::html_nodes(xpath = "//strong/a") %>%
-  rvest::html_attr("href")
 
-url2 <- paste("https://repositorio.unb.br", url, sep = "")
 
-html2 <- read
